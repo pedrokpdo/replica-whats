fef8d5e87d3a186515dd178af1e1dd1864e16757
pedrokpdo <pedromotoehm@gmail.com>
2022-10-11 18:38:13 -0300

    Verificações no MessageComponent

--- src/components/MessageComponent/index.tsx
+++ src/components/MessageComponent/index.tsx
@@ -3,10 +3,10 @@ import { FullCard } from '../funcionais/FullCard'
 import { Title } from '../funcionais/TitleComponent'
 import { View } from 'react-native'
 import { Row } from '../funcionais/RowComponent'
-export const MessageComponent = ({ message }) => {
+export const MessageComponent = ({ message, text }) => {
     return (
-        <Row style={{ alignSelf: 'start' }}>
-            <Title style={{ backgroundColor: message === 'me' ? 'white' : '#238152', padding: 16 }}>iansdinasind</Title>
+        <Row top={1} bottom={1} style={{ alignSelf: message === 'me' ? 'flex-start' : 'flex-end' }}>
+            <Title style={{ backgroundColor: message === 'me' ? 'white' : '#44be81', padding: 16, borderRadius: 8, color: message === 'me' ? 'black' : 'white' }}>{text}</Title>
         </Row>
     )
 }
--- src/screens/TcScreen/index.tsx
+++ src/screens/TcScreen/index.tsx
@@ -14,26 +14,30 @@ export const TcScreen = () => {
     return (
         <ContainerComponent style={{ backgroundColor: '#c7c0a6' }}>
             <FullCard style={{ backgroundColor: '#238152' }}>
-                <Row style={{ alignItems: 'center' }}>
+                <Row style={{ alignItems: 'center', justifyContent: 'space-between' }}>
                     <Row style={{ alignItems: 'center' }}>
                         <Feather name='arrow-left' size={24} color={'white'} />
-                        <Image source={profile} style={{ width: 50, height: 50, borderRadius: 30, marginHorizontal: 8 }}/>
+                        <Image source={profile} style={{ width: 50, height: 50, borderRadius: 30, marginHorizontal: 8 }} />
                         <Title bold style={{ fontSize: 24, color: 'white' }}>Pedro Henrique</Title>
                     </Row>
-                    <Row style={{ justifyContent: 'space-around', width: 200 }}>
-                        <AntDesign name='camera' color='white' size={24}/>
-                        <Feather name='phone-call' color='white' size={24}/>
-                        <SimpleLineIcons name='options-vertical' color='white' size={24}/>
+                    <Row style={{ justifyContent: 'space-around', width: 140 }}>
+                        <AntDesign name='camera' color='white' size={24} />
+                        <Feather name='phone-call' color='white' size={24} />
+                        <SimpleLineIcons name='options-vertical' color='white' size={24} />
                     </Row>
                 </Row>
             </FullCard>
             <Space top={2}>
                 <FullCard style={{ borderRadius: 8 }}>
-                <Text style={{ color: 'gray', textAlign: 'center' }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum itaque eligendi corrupti sapiente magnam, quisquam fugiat? Dolores ratione animi sint.</Text>
+                    <Text style={{ color: 'gray', textAlign: 'center' }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum itaque eligendi corrupti sapiente magnam, quisquam fugiat? Dolores ratione animi sint.</Text>
                 </FullCard>
             </Space>
             <Space>
-                <MessageComponent message={'me'}/>
+                <MessageComponent text='Ola amigo' message={'me'} />
+                <MessageComponent text='Vamos no shopping?' message={'me'} />
+                <MessageComponent text='Hoje não' message={'you'} />
+                <MessageComponent text='To muito ocupado' message={'you'} />
+                <MessageComponent text='Que pena, ta bom então p.p' message={'me'} />
             </Space>
         </ContainerComponent>
     )
