97834e4e1d834a63319dc21a202cf3e896e47dd7
pedrokpdo <pedromotoehm@gmail.com>
2022-10-11 15:25:04 -0300

    HeaderHome

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { AppStack } from './src/stacks/app.stack';
 import { NavigationContainer } from '@react-navigation/native'
+import { StatusBar } from 'react-native';
 
 export default function App() {
   return (
     <NavigationContainer>
+      <StatusBar translucent={false} backgroundColor='green'/>
       <AppStack />
     </NavigationContainer>
   );
--- src/components/HeaderHome/index.tsx
+++ src/components/HeaderHome/index.tsx
@@ -1,10 +1,16 @@
+import { AntDesign } from '@expo/vector-icons'
+import { SimpleLineIcons } from '@expo/vector-icons'
 import React from 'react'
-import { View } from 'react-native'
-
-export const HeaderHome = ( ) => {
+import { Row } from '../funcionais/RowComponent'
+import { Title } from '../funcionais/TitleComponent'
+export const HeaderHome = () => {
     return (
-        <View>
-           
-        </View>
+        <Row style={{ backgroundColor: '#238152', alignItems: 'center', justifyContent: 'space-between', padding: 16 }}>
+            <Title style={{ color: 'white', fontSize: 28 }}>WhatsApp</Title>
+            <Row style={{ alignItems: 'center', width: 100, justifyContent: 'space-around' }}>
+                <AntDesign name='search1' size={30} color={'white'} />
+                <SimpleLineIcons name='options-vertical' color='white' size={24} />
+            </Row>
+        </Row>
     )
 }
